4fdb63ea80439a5bb126250874264d8c2c065a8f
AshishPawar24 <vishnoiashish67@gmail.com>
2024-07-27 17:31:17 +0530

    Initial commit

---
+++ firstjavaprogram.java
@@ -0,0 +1,7 @@
+public class firstjavaprogram{
+    public static void main(String args[]){
+        System.out.println("hello world");
+        System.out.println("congratulations on ur first code oush on git hub");
+        
+    }
+}
