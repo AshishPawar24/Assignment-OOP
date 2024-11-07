034d1c3949f44b4ced62dae042ddf7e82b3b217a
AshishPawar24 <vishnoiashish67@gmail.com>
2024-11-07 16:53:52 +0530

    Delete firstjavaprogram.java

--- firstjavaprogram.java
+++
@@ -1,7 +0,0 @@
-public class firstjavaprogram{
-    public static void main(String args[]){
-        System.out.println("hello world");
-        System.out.println("congratulations on ur first code oush on git hub");
-        
-    }
-}
